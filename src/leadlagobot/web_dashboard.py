7ab8fa4f4fe1810cce560e09926b8dc0acdec57a
lucapicone <luka.picone@gmail.com>
2026-03-26 22:37:11 -0300

    fix: serve dashboard and data from repo root

--- src/leadlagobot/web_dashboard.py
+++ src/leadlagobot/web_dashboard.py
@@ -6,9 +6,16 @@ ROOT = Path(__file__).resolve().parents[2]
 WEB_DIR = ROOT / 'web'
 
 
+class DashboardHandler(SimpleHTTPRequestHandler):
+    def do_GET(self):
+        if self.path in ('/', '/index.html'):
+            self.path = '/web/index.html'
+        return super().do_GET()
+
+
 def main():
-    os.chdir(WEB_DIR)
-    server = ThreadingHTTPServer(('0.0.0.0', 8080), SimpleHTTPRequestHandler)
+    os.chdir(ROOT)
+    server = ThreadingHTTPServer(('0.0.0.0', 8080), DashboardHandler)
     print('LeadLagobot web dashboard on http://0.0.0.0:8080')
     server.serve_forever()
 
